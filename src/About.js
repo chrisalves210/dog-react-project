1437b2034f46e6af0c2318f33ee520c13cde1adc
christianalves <christian.alves@students.dominican.edu>
2026-03-04 13:43:05 -0800

    Add dog details pages and styling

--- src/About.js
+++ src/About.js
@@ -7,11 +7,12 @@ function About() {
         <h1>About This App</h1>
         <p>
           This simple React app shows a few popular dog breeds and some of
-          their key traits.
+          their key traits and also has photos of each breed.
         </p>
       </header>
     </div>
   );
 }
 
-export default About;
+export default About;export function BreedCard({ id, name, size, traits, image: images }) { }
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { useState } from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
-import About from './About';
+import { Link, Routes, Route, useParams } from 'react-router-dom';
+import About, { BreedCard } from './About';
 import dogBreeds from './dogBreeds';
 
-function BreedCard({ name, size, traits, image }) {
+/ }) {
   return (
     <li className="breed-card">
       {image && (
@@ -17,7 +17,50 @@ function BreedCard({ name, size, traits, image }) {
       <p>
         <strong>Traits:</strong> {traits}
       </p>
+      <Link to={`/breed/${id}`} className="breed-link">
+        Learn more
+      </Link>
     </li>
+  );
+}
+
+function BreedDetails() {
+  const { id } = useParams();
+  const breed = dogBreeds.find((b) => b.id === id);
+
+  if (!breed) {
+    return <p>Sorry, that dog breed was not found.</p>;
+  }
+
+  return (
+    <div className="breed-details">
+      {breed.image && (
+        <img src={breed.image} alt={breed.name} className="breed-details-image" />
+      )}
+      <h1>{breed.name}</h1>
+      <p>
+        <strong>Size:</strong> {breed.size}
+      </p>
+      {breed.weight && (
+        <p>
+          <strong>Weight:</strong> {breed.weight}
+        </p>
+      )}
+      {breed.origin && (
+        <p>
+          <strong>Origin:</strong> {breed.origin}
+        </p>
+      )}
+      {breed.lifespan && (
+        <p>
+          <strong>Lifespan:</strong> {breed.lifespan}
+        </p>
+      )}
+      {breed.description && <p className="breed-description">{breed.description}</p>}
+      <Link to="/" className="back-link">
+        ← Back to all breeds
+      </Link>
+    </div>
   );
 }
 
@@ -43,6 +86,7 @@ function Home() {
         {filteredBreeds.map((breed) => (
           <BreedCard
             key={breed.name}
+            id={breed.id}
             name={breed.name}
             size={breed.size}
             traits={breed.traits}
@@ -64,6 +108,7 @@ function App() {
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/breed/:id" element={<BreedDetails />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </header>
